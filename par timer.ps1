a79525996c27f0f352add04cfacb2ff589958d83
jrbdizon <50459326+jrbdizon@users.noreply.github.com>
2022-11-08 01:05:57 -0500

    Add files via upload

---
+++ par timer.ps1
@@ -0,0 +1,13 @@
+﻿#Beep(Beep Tone, Beep length in ms) max 500 is good for short, 1000 is for long. 
+$Delay = Get-Random -Minimum 2.0 -Maximum 3.5
+$ParTime = 2
+While($True){
+$Response = [System.Windows.MessageBox]::Show('Run Par Timer?','Par Timer','YesNo','Asterisk')
+    if($Response -eq 'No'){
+        Break
+    }
+    Start-Sleep -seconds $Delay
+    [Console]::Beep(1000, 300)
+    Start-Sleep -seconds $ParTime
+    [Console]::Beep(1000, 1000)
+}
